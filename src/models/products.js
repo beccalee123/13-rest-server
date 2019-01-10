b2d98be4df9349ad0a0b2f1613f2260512cf53c4
BECCA LEE <REBECCA.KIRSTIN.LEE@GMAIL.COM>
2019-01-09 20:11:22 -0800

    Adds product modules

--- src/models/products.js
+++ src/models/products.js
@@ -1,9 +1,6 @@
 'use strict';
 
-const uuid = require('uuid/v4');
-
-const schema = {
-};
+const product = require('./product-schema.js');
 
 class Products {
 
@@ -11,21 +8,40 @@ class Products {
     this.database = [];
   }
 
-  get(id) {
+  get(_id) {
+    let queryObject = _id ? {_id} : {};
+    return product.find(queryObject);
   }
   
   post(entry) {
+    let newRecord = new product(entry);
+    return newRecord.save();
   }
 
   put(id, entry) {
+    entry._id = id;
+    return product.update({'_id': id}, {$set:{'name':entry.name, 'description':entry.description}});
   }
 
   delete(id) {
-  }
-
-  sanitize(entry) {
+    return product.deleteOne({'_id': id});
   }
 
 }
+
+// put(_id, entry) {
+//   for(let i = 0; i < this.database.length; i++){
+//     if(entry._id === this.database[i]['_id']){
+//       this.database[i] = record;
+//     }
+//   }
+//   return Promise.resolve(record);
+// }
+
+// delete(_id) {
+//   this.database = this.database.filter((record) => record._id !== _id);
+//   return Promise.resolve({});
+// }
+
 
 module.exports = Products;
